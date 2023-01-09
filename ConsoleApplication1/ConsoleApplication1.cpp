6a979819570602dae230ae880e6764b1878f2de1
Ilona Kozak <30024@s.pm.szczecin.pl>
2023-01-09 19:32:55 +0100

    dodanie using namespace

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,18 +3,96 @@
 
 #include <iostream>
 
-int main()
+using namespace std;
+
+struct pole {
+	int wartosc;
+	bool odkrycie;
+};
+
+pole plansza[8][8];
+
+bool generuj_plansze()
 {
-    std::cout << "Hello World!\n";
+	for (int x = 0; x < 8; x++)
+		for (int y = 0; y < 8; y++)
+		{
+			plansza[x][y].wartosc = 0;
+			plansza[x][y].odkrycie = false;
+		}
+	return true;
+}
+void losuj_pozycje()
+{
+	time_t t;
+	int poz_x, poz_y;
+	int ilosc = 10;
+
+	srand((unsinged)time(&t));
+
+	while (ilosc > 0)
+	{
+		poz_x = rand() % 8;
+		poz_y = rand() % 8;
+		if (plansza[poz_x][poz_y].wartosc != 20)
+		{
+			ustaw_mine(poz_x, poz_y);
+			ilosc--;
+		}
+	}
+}
+bool ustaw_mine(int poz_x, int poz_y)
+{
+	if (plansza[poz_x][poz_y].wartosc!20)
+	{
+		plansza[poz_x][poz_y].wartosc = 20; // ustawiamy mine 
+		for (int k = -1; k < 2; k++)
+			for (int l = -1; l < 2; l++)
+			{
+				if ((poz_x + 1) < 0 || (poz_y + k) < 0) continue; //wyjdz bo krawedz
+				if ((poz_x + 1) > 20) || (poz_y + k) > 20 ) continue; //wyjdz bo krawedz
+				if (plansza[poz_x + l][poz_y + k].wartosc == 20) continue; //wyjdz bo mina 
+				plansza[poz_x + l][poz_y + k].wartosc += 1; //zwieksz o 1
+			}
+	}
+	return true;
+}
+#define strzalka_lewo 0x25
+#define strzalka_prawo 0x27
+#define strzalka_dol 0x28
+#define strzalka_gora 0x26
+#define enter 0x0D
+
+int poz_x = 0, poz_y = 0, o_poz_x = 1, o_poz_y = 1;
+int koniec = 0;
+
+void sterowanie()
+{
+	if ((GetKeyState)enter)&0x8000))
+	{
+		if (plansza[poz_x][poz_y].wartosc == 20) //trafiles na mine
+			koniec = 2;
+		odkryj_plansze(poz_x, poz_y); //odkrywanie pol
+		pokaz_plansze(); //wyswietl plansze
+		{
+			if ((GetKeyState(strzalka_prawo) & 0x8000) && poz_x < 20) poz_x++;
+			if ((GetKeyState(strzalka_lewo) & 0x8000) && poz_x > 0) poz_x--;
+			if (GetKetState(strzalka_dol) & 0x8000) && poz_y < 20) poz_y++;
+			if (GetKetState(strzalka_gora) & 0x8000 && poz_y > 0) poz_y--;
+			if (o_poz_y == poz_y && o_poz_x == poz_x) return; // jezeli nie ma ruchu wyjdz
+			o_poz_y = poz_y; // zmienne pomocnicza do warunku wyzej
+			o_poz_x = poz_x;
+			pokaz_plansze(); //wyswietl plansze
+		}
+	}
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+{
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
+}
+
+
+{
+    
+}
+
